a135e4c4e8583b1009df62d8da944857ea6b21a6
gpu governor <gpu.governor@gmail.com>
2025-01-10 00:28:30 +0100

    fixed anoyning

--- Game/main.py
+++ Game/main.py
@@ -10,11 +10,11 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(GAME_CAPTION)
 clock = pygame.time.Clock()
 
-player = AnimatedActor(["Data/img/player/Idle/0.png", "Data/img/player/Idle/1.png", "Data/img/player/Idle/2.png", "img/Idle/3.png"],100, 100,2.0,3)
+player = AnimatedActor(["Data/img/player/Idle/0.png", "Data/img/player/Idle/1.png", "Data/img/player/Idle/2.png", "Data/img/player/Idle/3.png"],100, 100,2.0,3)
 
 # EVENT LOOP
 while True:
-    player.render(screen)
+    player.render(screen,50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
