192a801efe1ece8a17ac042e242ca63b8c844750
gpu governor <gpu.governor@gmail.com>
2025-01-10 00:59:15 +0100

    finally

--- Game/main.py
+++ Game/main.py
@@ -10,20 +10,24 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(GAME_CAPTION)
 clock = pygame.time.Clock()
 
-player = AnimatedActor(["Data/img/player/Idle/0.png", "Data/img/player/Idle/1.png", "Data/img/player/Idle/2.png", "Data/img/player/Idle/3.png"],100, 100,2.0,3)
-
+player_idle =["Data/img/player/Idle/0.png", "Data/img/player/Idle/1.png", "Data/img/player/Idle/2.png", "Data/img/player/Idle/3.png", "Data/img/player/Idle/4.png"]
+player_walk = ["Data/img/player/Run/0.png","Data/img/player/Run/1.png","Data/img/player/Run/2.png","Data/img/player/Run/3.png","Data/img/player/Run/4.png","Data/img/player/Run/5.png"]
+player = AnimatedActor(player_idle,300, 300, 2.0, 0.1)
 # EVENT LOOP
 while True:
-    player.render(screen,50)
+    delta_time = clock.tick(60) / 1000.0  # Time in seconds
+    screen.fill((128,128,128))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                pass          
-            if event.key == pygame.K_DOWN:
-                pass
+            if event.key == pygame.K_RIGHT:
+                player.file_path = player_walk
+                player.x = player.x+2       
+            if event.key == pygame.K_LEFT:
+                player.x = player.x-2
 
+    player.render(screen,delta_time)
     pygame.display.update()
     clock.tick(60)
